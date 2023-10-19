d66c50ed73f069a4c7d026adf490b14376c30800
Siarhei Prymakovich <prima.psa@gmail.com>
2023-10-19 19:23:30 +0300

    feat: add styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
+
 import { StrictMode } from 'react'
-
 import { createRoot } from 'react-dom/client'
-
-import './index.css'
-
 import { App } from './App'
+import './index.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/700.css'
+import './styles/index.scss'
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
